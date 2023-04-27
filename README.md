e415298d6a05fc21339f92ba1913a369265fd5f6
Esayas Asefa <esayas.t.asefa@gmail.com>
2023-04-27 16:39:54 -0500

    __

---
+++ README.md
@@ -0,0 +1,82 @@
+# classification_churn_project
+
+# Churn Drivers
+
+Project Description:
+    
+    Reducing churn is a perpetual challenge for non-monopoly client based businesses. Finding out what factors lead to churn will provide Telco an opportunity to reduce the causes of churn. 
+    
+## Project Goal:
+
+     Identify feature(s) that drive churn
+     Use those features/drivers of churn to develop a model that would identify who is most likely to churn
+     The information could be used to retain clientele and maybe even increase Telco's client base
+    
+## Initial Thoughts 
+
+    My initial hypothesis is that drivers of churn will be elements that cause inconvenience or increase in cost.
+   
+## The Plan
+
+     Acquire data from personal
+     Prepare data
+        From existing data
+             Identify nulls/Nan and replace them with the mean or get rid of that column if there are too many nulls/Nans
+             Idenfity non-numerical columns and convert them into classification columns (1 and 0)
+     
+## Explore data
+
+     Answer the following initial questions:
+           Is one gender more likely to affect churn than the other?
+           Are senior citizens more likely to churn?
+           Do people with dependents churn more?
+           Do people with partners churn more?
+            
+## Develop a Model to predict churn
+
+      Use drivers identified in explore to build predictive models of different types
+      Evaluate models on train and validate data
+      Select the best model based on the highest accuracy
+      Evaluate the best model on test data
+
+ ## Data Dictionary
+      
+| Feature | Definition |
+|:--------|:-----------|
+|tenure| How many months the customer has been a customer|
+|monthly_charges| How much the customer pays monthly|
+|Contract type ID| The total the customer has paid|
+|gender_Male| Whether the customer is male or not (0:no, 1:yes)|
+|senior_citizen_Yes| Whether the customer is a senior citizen or not 0:no, 1:yes|
+|partner_Yes| Whether the customer has a partner or not (0:no, 1:yes)|
+|dependents_Yes| Whether the customer has dependents or not (0:no, 1:yes)|
+|phone_service_Yes| Whether the customer has phone service or not (0:no, 1:yes)|
+|multiple_lines_No phone service| Whether the customer has multiple lines of service w/o phone service|
+|multiple_lines_Yes| Whether the customer has multiple lines of service or not|
+|online_security_No internet service|Whether the customer has online security w/o internet service or not|
+|online_security_Yes|Whether the customer has online security or not|
+|online_backup_No internet service|Whether the customer has online backup w/o internet service or not|
+|online_backup_Yes|Whether the customer has online backup or not|
+|device_protection_No internet service|Whether the customer has device protection w/o internet service or not|
+|device_protection_Yes|Whether the customer has device protection or not|
+|tech_support_No internet service|Whether the customer has tech support w/o internet service or not|
+|tech_support_Yes|Whether the customer has tech support or not|
+|streaming_tv_No internet service|Whether the customer has tv streaming services w/o internet service or not|
+|streaming_tv_Yes|Whether the customer has tv streaming services or not|
+|streaming_movies_No internet service|Whether the customer has movie streaming services w/o internet services or not|
+|streaming_movies_Yes|Whether the customer has movie streaming services or not|
+|paperless_billing_Yes|Whether the customer is enrolled in paperless billing or not|
+|churn_Yes|Whether the customer churned or not 0:no, 1:yes|
+
+
+## Steps to Reproduce
+
+    1. Clone this repo
+    2. Acquire the data form Kaggle
+    3. Put the data in the file containing the cloned repo
+    4. Run notebook
+    
+## Takeaways and Conclusions
+
+
+## Recommendations
---
+++ wrangle.py
@@ -0,0 +1,132 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+
+from termcolor import colored
+
+
+import env
+import numpy as np
+
+
+def check_file_exists(fn, query, url):
+    """
+    check if file exists in my local directory, if not, pull from sql db
+    return dataframe
+    """
+    if os.path.isfile(fn):
+        print('csv file found and loaded')
+        return pd.read_csv(fn, index_col=0)
+    else:
+        print('creating df and exporting csv')
+        df = pd.read_sql(query, url)
+        df.to_csv(fn)
+        return df
+
+def get_titanic_data():
+    url = env.get_connection_url('titanic_db')
+    filename = 'titanic.csv'
+    query = 'select * from passengers'
+
+    df = check_file_exists(filename, query, url)
+
+    return df
+
+
+def get_iris_data():
+    url = env.get_connection_url('iris_db')
+    query = '''
+            select * from measurements
+                join species
+                    using (species_id)
+            '''
+    filename = 'iris.csv'
+    df = check_file_exists(filename, query, url)
+    return df
+
+
+
+def get_telco_churn():
+    url = env.get_connection_url('telco_churn')
+    query = ''' select * from customers
+	join contract_types
+		using (contract_type_id)
+	join internet_service_types
+		using (internet_service_type_id)
+	join payment_types
+		using (payment_type_id)
+        '''
+    filename = 'telco.csv'
+    df = check_file_exists(filename, query, url)
+
+    return df
+
+def prep_iris():
+    iris = get_iris_data()
+    iris = iris.drop(columns=['species_id', 'measurement_id'])
+    iris = iris.rename(columns={'species_name': 'species'})
+    num=iris.select_dtypes(include="number")
+    char=iris.select_dtypes(include="object")
+    char_1 = pd.get_dummies(char)
+    char = pd.concat([char, char_1], axis=1)
+    iris_clean = pd.concat([num, char], axis=1)
+    return iris_clean
+
+
+
+def prep_telco():
+    telco = get_telco_churn()
+    telco = telco.drop(columns=['customer_id', 'payment_type_id', 'internet_service_type_id', 'contract_type_id'])
+    telco['senior_citizen'] = telco['senior_citizen'].replace({0: 'No', 1: 'Yes'})
+    telco['total_charges'] = telco['total_charges'].replace({' ': 0})
+    telco['total_charges'] = telco['total_charges'].astype('float')
+    num=telco.select_dtypes(include="number")
+    char=telco.select_dtypes(include="object")
+    char_1 = pd.get_dummies(char, drop_first=True)
+    char = pd.concat([char, char_1], axis=1)
+    telco_clean = pd.concat([num, char_1], axis=1)
+    return telco_clean
+
+def prep_titanic(titanic):
+    titanic = titanic.drop(columns=['passenger_id', 'sibsp', 'sex', 'parch', 'embarked', 'class', 'deck', 'embark_town', 'alone'])
+    dummy_df = pd.get_dummies(data=titanic[['sex','embark_town']], drop_first=True)
+    titanic = pd.concat([titanic], axis=1)
+    
+    return titanic
+
+def test_train(df, target):
+
+    temp_train, test = train_test_split(df,  test_size= .2, random_state=123, stratify=df[target])
+    train, validate = train_test_split(temp_train, test_size= .25, random_state=123, stratify=temp_train[target])
+    return train, validate, test
+
+def eval_results(p):
+    alpha = .05
+    if p < alpha:
+        print("We reject the null hypothesis")
+    else:
+        print("We fail to reject the null hypothesis")
+def prep_data(df):
+    sum_null = df.isnull().sum()
+    df = df.drop(columns=sum_null[sum_null > 0].index)
+    print(colored(f'Columns with null values: {sum_null[sum_null > 0].index}', 'red'))
+    return df
+
+def check_nulls(df):
+    return df.isnull().sum()
+
+def check_duplicates(df):
+    if df.nunique().sum() == df.shape[0]:
+        print('No duplicates')
+    else:
+        print('Duplicates exist')
+
+def train_validate_test_split(X_all, Y):
+    '''
+    This function takes in a dataframe, the target variable, and a seed for reproducibility.
+    It will split the data into train, validate, and test datasets.
+    '''
+
+    X_train, X_test, y_train, y_test = train_test_split(X_all, Y, test_size=0.2, random_state=123, stratify=Y)
+    X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, test_size=0.25, random_state=123, stratify=y_train)
+    return X_train, X_validate, X_test, y_train, y_validate, y_test
